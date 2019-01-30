57f1e41942a922b0744c9d42ad5daf8fb337de3f
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2019-01-29 19:12:06 -0600

    created sample react pages content

--- client/src/index.js
+++ client/src/index.js
@@ -7,10 +7,13 @@ import {Provider} from "react-redux";
 import store from "./redux/store";
 //component import
 import App from './App';
+import {BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
     <Provider>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
