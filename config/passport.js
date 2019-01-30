02797432caa4c18b0b5c74731040ffe5bc47cdc2
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2019-01-29 23:32:00 -0600

    updated passport for username use

--- config/passport.js
+++ config/passport.js
@@ -3,26 +3,22 @@ var LocalStrategy = require("passport-local").Strategy;
 
 var db = require("../models");
 
-// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
+// Telling passport we want to use a Local Strategy. In other words, we want login with a username and password
 passport.use(new LocalStrategy(
-  // Our user will sign in using an email, rather than a "username"
-  {
-    usernameField: "email"
-  },
-  function(email, password, done) {
+  function(username, password, done) {
     // When a user tries to sign in this code runs
     db.User.findOne({
       where: {
-        email: email
+        username: username
       }
     }).then(function(dbUser) {
-      // If there's no user with the given email
+      // If there's no user with the given username
       if (!dbUser) {
         return done(null, false, {
-          message: "Incorrect email."
+          message: "Incorrect username."
         });
       }
-      // If there is a user with the given email, but the password the user gives us is incorrect
+      // If there is a user with the given username, but the password the user gives us is incorrect
       else if (!dbUser.validPassword(password)) {
         return done(null, false, {
           message: "Incorrect password."
