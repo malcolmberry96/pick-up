87f7dc02e4511c5c607539cf9b6d8e7442634bcd
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2019-02-02 10:36:34 -0600

    troubleshooting routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,16 +3,16 @@ var db = require("../models");
 var passport = require("../config/passport");
 
 module.exports = function(app) {
-    // Using the passport.authenticate middleware with our local strategy.
-    // If the user has valid login credentials, send them to the members page.
-    // Otherwise the user will be sent an error
-    app.post("/api/login", passport.authenticate("local"), function(req, res) {
-        // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
-        // So we're sending the user back the route to the members page because the redirect will happen on the front end
-        // They won't get this or even be able to access this page if they aren't authed
-        // res.json("/members");
-        console.log("Successfully logged-in.");
-    });
+  // Using the passport.authenticate middleware with our local strategy.
+  // If the user has valid login credentials, send them to the members page.
+  // Otherwise the user will be sent an error
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+      // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
+      // So we're sending the user back the route to the members page because the redirect will happen on the front end
+      // They won't get this or even be able to access this page if they aren't authed
+      // res.json("/members");
+      console.log("Successfully logged-in.");
+  });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
