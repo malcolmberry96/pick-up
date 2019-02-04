6d7c5b2aa812a68b33d7f2ef29bbab8b22e299cf
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2019-02-04 03:33:13 -0600

    added back in socket references and added to socket routes

--- routes/sockets.js
+++ routes/sockets.js
@@ -1,8 +1,17 @@
 module.exports = function(io, app) {
     io.on("connection", (socket) => {
         console.log(`A user connected to the socket: ${socket}`);
-        socket.on("disconnect", () => {
-            console.log("User disconnected");
+        socket.on("order successfully submitted", () => {
+            console.log(`value of socket is: ${socket}`);
+            socket.broadcast.emit("new order", {newOrder: true});
+        });
+        socket.on("driver status change", (statusData) => {
+            console.log(JSON.stringify(statusData));
+            io.emit("new driver status", {driverStatus: statusData});
+        });
+        socket.on("order status change", (orderData) => {
+            console.log(JSON.stringify(orderData));
+            io.emit("new order status", {orderStatus: orderData});
         });
     });
 };
