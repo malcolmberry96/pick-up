f496154e17008854aeb3a3f05b403f6b34c96de3
riffon3000 <riffon@gmail.com>
2019-02-03 19:31:38 -0600

    google maps

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MapContainer from './MapContainer';
+// import App from './App';
+// import {BrowserRouter as Router} from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 // redux setup
 // import {Provider} from "react-redux";
 // import store from "./redux/store";
 //component import
-import App from './App';
-import {BrowserRouter as Router} from "react-router-dom";
+
 
 ReactDOM.render(
     // <Provider>
-        <Router>
-            <App />
-        </Router>,
+    <MapContainer />,
+    // <Router>
+    //     <App />
+    // </Router>,
     // </Provider>,
     document.getElementById('root')
 );
